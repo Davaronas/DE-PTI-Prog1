1e40657692ec3c1b172d246e6aa9eef184b4c3b8
Davaronas <43610875+Davaronas@users.noreply.github.com>
2019-03-12 19:42:15 +0100

    Update 3.8.c

--- 3.8.c
+++ 3.8.c
@@ -2,19 +2,26 @@
 
 int main()
 {
+// Egész  
 int a;
 
+// Egészre mutató mutató 
 int *b;
 
+// Egész referenciája
 int &&c = 5;
 
+// Egészek tömbje
 int d[5] = {3,2,1,5,6};
 
+// Egészek tömbjének referenciája
 int* &&e = d;
 
+// Egészre mutató mutatók tömbje
 int *f[5];
 }
 
+// Egészre mutató mutatót visszaadó függvény
 int AA()
 {
 
@@ -24,6 +31,7 @@ return *b1;
 
 }
 
+// Egészre mutató mutatót visszaadó függvényre mutató mutató
 int (*CC)(int);
 
 int BB()
