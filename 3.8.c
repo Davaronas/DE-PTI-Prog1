b8d2b230a1e1ce7116fd2a38824721dd1551e525
Davaronas <43610875+Davaronas@users.noreply.github.com>
2019-03-12 19:24:01 +0100

    3.8.c

---
+++ 3.8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+int a;
+
+int *b;
+
+int &&c = 5;
+
+int d[5] = {3,2,1,5,6};
+
+int* &&e = d;
+
+int *f[5];
+}
+
+int AA()
+{
+
+int *b1;
+
+return *b1;
+
+}
+
+int (*CC)(int);
+
+int BB()
+{
+}
