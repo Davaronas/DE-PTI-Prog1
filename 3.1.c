c9c30d7c314d09073647954dc142b9f0a3f480c1
Davaronas <43610875+Davaronas@users.noreply.github.com>
2019-04-10 08:34:41 +0200

    Update 3.1.c

--- 3.1.c
+++ 3.1.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
-
 int main()
 {
+    printf("Kérek egy decimális számot! ");
+    char decimal[1024];
+    scanf("%s", decimal);
+    char unary[1024];
+    char szamok[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    int hely = 0;
+    int hely1 = 0;
+    int hely3 = 0;
+    printf("\n\n");
+    while(decimal[0] != '0')
+    {
+        while(decimal[hely+1] != 0)
+            hely++;
 
-	int szam = 0;
-	scanf("%d", &szam);
-	for (int i = 0; i < szam; i++)
-	{
-		printf("'");
-	}
-	printf("\n");
+        while(decimal[hely] != szamok[hely1])
+            hely1++;
+
+        if(szamok[hely1] != '0')
+        {
+            hely1--;
+            decimal[hely] = szamok[hely1];
+        }
+        else
+        {
+            hely1 = 9;
+            decimal[hely] = szamok[hely1];
+        }
+        unary[hely3] = '1';
+        hely3++;
+
+        printf("%s\n", decimal);
+        printf("%s\n", unary);
+    }
+
+    return 0;
 }
